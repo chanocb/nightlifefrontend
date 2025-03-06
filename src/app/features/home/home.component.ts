0f6bf8a89b9ea93fc2d07b3b2ed15056bb19025e
JAVIER ARANDA LOPEZ <javier.arandal@alumnos.upm.es>
2025-03-06 19:59:00 +0100

    Update validation form register  #5

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,14 +1,12 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { User } from '../../core/models/user.model';
 import { Role } from '../../core/models/role.model';
-import { NgFor, NgIf } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-//import {AuthService} from "@core/services/auth.service";
 
 @Component({
   selector: 'app-home',
   standalone:true,
-  imports: [NgIf, NgFor, FormsModule],
+  imports: [ FormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
--- src/app/features/register/register.component.ts
+++ src/app/features/register/register.component.ts
@@ -2,19 +2,19 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { User } from '../../core/models/user.model';
 import { Role } from '../../core/models/role.model';
 import { AsyncPipe, NgFor, NgIf } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import {AuthService} from "@core/services/auth.service";
+import { FormBuilder, FormsModule  } from '@angular/forms';
+import { AuthService } from "@core/services/auth.service";
 import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-register',
-  standalone:true,
+  standalone: true,
   imports: [NgIf, NgFor, FormsModule, AsyncPipe],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class RegisterComponent { 
+export class RegisterComponent {
   user: User = {
     email: '',
     password: '',
@@ -24,14 +24,23 @@ export class RegisterComponent {
     birthDate: '',
     role: Role.CLIENT
   };
+  confirmPassword!: String;
   roles: Role[] = [Role.OWNER, Role.CLIENT];
   
   registeredUser$!: Observable<User>;
   
-  constructor(private authService: AuthService) {}
-
-  register(): void {
-    this.registeredUser$ = this.authService.registerUser(this.user);
+  constructor(private authService: AuthService, private fb: FormBuilder) {
+    
   }
 
+  // Validación del email con expresión regular
+  emailValid(): boolean {
+    const emailPattern = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/;
+    return emailPattern.test(this.user.email);
+  }
+
+  register(): void {
+    
+    this.registeredUser$ = this.authService.registerUser(this.user);
+  }
 }
