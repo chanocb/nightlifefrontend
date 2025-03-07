b550550a3520d746ed6075b19ddaea1c49ff2c80
JAVIER ARANDA LOPEZ <javier.arandal@alumnos.upm.es>
2025-03-07 18:34:56 +0100

    Fix port in environment  #5

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,5 @@ export const environment = {
     production: false,
     NAME: pkg.name,
     VERSION: pkg.version,
-    REST: 'http://host.docker.internal:8082', //NOSONAR
+    REST: 'http://host.docker.internal:8081', //NOSONAR
 };
