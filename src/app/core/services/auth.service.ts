3ba727810c150c742a1c5014abc78fd24be0fa48
Chano <chano@correo.ugr.es>
2025-03-08 20:09:54 +0100

    Added http.service.ts and app-error.model.ts #5

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,15 +1,15 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { User } from '@core/models/user.model';
 import {environment} from '@env';
 import { Observable } from 'rxjs';
+import { HttpService } from './http.service';
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   static readonly END_POINT = environment.REST + '/users'
-  constructor(private http: HttpClient) { }
+  constructor(private readonly httpService: HttpService) { }
   registerUser(user: User): Observable<User> {
-    return this.http.post<User>(AuthService.END_POINT, user);
+    return this.httpService.post(AuthService.END_POINT, user);
   }
 }
