b4be280f36fb537f8f00b0d6abaa23ba00e2336f
JAVIER ARANDA LOPEZ <javier.arandal@alumnos.upm.es>
2025-03-16 18:28:06 +0100

    Fix logout #5

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -2,11 +2,14 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { User } from '../../core/models/user.model';
 import { Role } from '../../core/models/role.model';
 import { FormsModule } from '@angular/forms';
+import { AuthService } from '@core/services/auth.service';
+import { CommonModule } from '@angular/common';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone:true,
-  imports: [ FormsModule],
+  imports: [ RouterLink, CommonModule, FormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -22,4 +25,10 @@ export class HomeComponent {
     role: Role.CLIENT
   };
   roles: Role[] = [Role.OWNER, Role.CLIENT];
+
+  constructor(private router: Router,private authService: AuthService) {}
+
+  isAuthenticated(): boolean {
+    return this.authService.isAuthenticated();
+  }
 }
