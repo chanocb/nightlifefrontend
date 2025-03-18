fb241966fa535300e126b15b99c01f30855acded
Chano <chano@correo.ugr.es>
2025-03-18 20:33:33 +0100

    Added interceptor and localstorage #7

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,13 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http'; // ✅ Importa provideHttpClient
-
+import { provideHttpClient, withInterceptors } from '@angular/common/http'; // ✅ Importa provideHttpClient
+import { tokenInterceptor } from '@core/interceptors/token.interceptor';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
+    provideHttpClient(withInterceptors([tokenInterceptor])),
     provideRouter(routes),
-    provideHttpClient(), 
   ]
 };
--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,87 +1,90 @@
 import { Injectable } from '@angular/core';
 import { User } from '@core/models/user.model';
-import {environment} from '@env';
+import { environment } from '@env';
 import { BehaviorSubject, map, Observable } from 'rxjs';
 import { HttpService } from './http.service';
-import {JwtHelperService} from '@auth0/angular-jwt';
+import { JwtHelperService } from '@auth0/angular-jwt';
 import { Router } from '@angular/router';
+
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
   static readonly END_POINT = environment.REST + '/users';
   private user: any;
   private isAuthenticatedSubject = new BehaviorSubject<boolean>(false);
   isAuthenticated$ = this.isAuthenticatedSubject.asObservable();
-  constructor(private readonly httpService: HttpService, private readonly router: Router) { }
+
+  constructor(private readonly httpService: HttpService, private readonly router: Router) {
+    this.loadUserFromStorage();
+  }
+
+  private loadUserFromStorage(): void {
+    const userFromStorage = localStorage.getItem('user');
+    if (userFromStorage) {
+      this.user = JSON.parse(userFromStorage);
+      this.isAuthenticatedSubject.next(true);
+    }
+  }
+
   registerUser(user: User): Observable<User> {
     return this.httpService.post(AuthService.END_POINT, user);
   }
 
   login(email: string, password: string): Observable<User> {
-    return this.httpService.authBasic(email,password)
-    .post(AuthService.END_POINT + "/token")
-    .pipe(
-      map(jsonToken => {
+    return this.httpService
+      .authBasic(email, password)
+      .post(AuthService.END_POINT + '/token')
+      .pipe(
+        map((jsonToken) => {
           const jwtHelper = new JwtHelperService();
-          this.user = jsonToken; 
-          this.user.email = jwtHelper.decodeToken(jsonToken.token).user;  
+          this.user = jsonToken;
+          this.user.email = jwtHelper.decodeToken(jsonToken.token).user;
           this.user.name = jwtHelper.decodeToken(jsonToken.token).name;
           this.user.role = jwtHelper.decodeToken(jsonToken.token).role;
-          this.isAuthenticatedSubject.next(true); // Notificar a los observadores
+          localStorage.setItem('user', JSON.stringify(this.user));
+          this.isAuthenticatedSubject.next(true);
           return this.user;
-      })
-  );
-}
-
-logout(): void {
-  this.user = undefined;
-  this.isAuthenticatedSubject.next(false); // Notificar que el usuario cerró sesión
-  this.router.navigate(['']).then();
-}
-
-isAuthenticated(): boolean {
-  return this.user != null && !(new JwtHelperService().isTokenExpired(this.user.token));
-}
-
-getProfile(): Observable<User> {
-  const token = this.user?.token;
-  const email = this.user?.email;
-
-  console.log('Obteniendo perfil con:', { token, email });
-
-  if (!token || !email) {
-    console.error('Error: Usuario no autenticado');
-    throw new Error('Usuario no autenticado');
+        })
+      );
   }
 
-  return this.httpService
-    .header('Authorization', `Bearer ${token}`)
-    .get(`${AuthService.END_POINT}/${email}`);
-  
+  logout(): void {
+    this.user = undefined;
+    this.isAuthenticatedSubject.next(false);
+    localStorage.removeItem('user');
+    this.router.navigate(['']).then();
+  }
+
+  isAuthenticated(): boolean {
+    const userFromStorage = localStorage.getItem('user');
+    if (userFromStorage) {
+      this.user = JSON.parse(userFromStorage);
+      const jwtHelper = new JwtHelperService();
+      return !jwtHelper.isTokenExpired(this.user.token);
+    }
+    return false;
+  }
+
+  getProfile(): Observable<User> {
+    const email = this.user?.email;
+    if (!email) {
+      console.error('Error: Usuario no autenticado');
+      throw new Error('Usuario no autenticado');
+    }
+    return this.httpService.get(`${AuthService.END_POINT}/${email}`);
   }
 
   updateProfile(user: User): Observable<User> {
-    const token = this.user?.token;
-  const email = this.user?.email;
-
-  console.log('Obteniendo perfil con:', { token, email });
-
-  if (!token || !email) {
-    console.error('Error: Usuario no autenticado');
-    throw new Error('Usuario no autenticado');
+    const email = this.user?.email;
+    if (!email) {
+      console.error('Error: Usuario no autenticado');
+      throw new Error('Usuario no autenticado');
+    }
+    return this.httpService.put(`${AuthService.END_POINT}/${email}`, user);
   }
 
-  return this.httpService
-    .header('Authorization', `Bearer ${token}`)
-    .put(`${AuthService.END_POINT}/${email}`, user);
-  
+  getToken(): string {
+    return this.user ? this.user.token : undefined;
   }
-    //return this.httpService.put(`${AuthService.END_POINT}/${email}`, user);
-  //}
 }
-
-  
-
-  
-
